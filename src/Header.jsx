7aae1c16d2cbe9660d7abdb8e217aa986f1afdb4
Fabio Pichler <fabiopichler1992@gmail.com>
2020-01-15 00:43:26 -0300

    updated

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import clsx from 'clsx';
 
-const Header = () => (
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+
+const Header = ({
+    game,
+}) => (
     <header className="header my-3">
         <h1 className="text-center">Tic Tac Toe</h1>
+
+        <h5 className="text-center mb-0">Simples jogo da velha em React</h5>
+
+        <div className="footer-players">
+            <div className={clsx({ selected: (game.player === 1 && game.winner === 0) || game.winner === 1 })}>
+                <FontAwesomeIcon
+                    icon={faSun}
+                    className="mr-2"
+                />
+
+                Player 1
+            </div>
+
+            <div className={clsx({ selected: (game.player === 2 && game.winner === 0) || game.winner === 2 })}>
+                <FontAwesomeIcon
+                    icon={faMoon}
+                    className="mr-2"
+                />
+                
+                Player 2
+            </div>
+        </div>
     </header>
 );
 
