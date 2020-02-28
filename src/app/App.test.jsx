a8e05404a3a1e8c247b26b34b2f50242d87f57cf
Fabio Pichler <fabiopichler1992@gmail.com>
2020-02-27 21:50:01 -0300

    update tests

--- src/app/App.test.jsx
+++ src/app/App.test.jsx
@@ -8,9 +8,37 @@ import App, {
     check,
 } from './App';
 
+test('transpose', () => {
+    expect(transpose([
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 0, 0],
+    ])).toEqual([
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0],
+    ]);
+});
+
 test('checkRow', () => {
     expect(checkRow([1, 1, 1], 1)).toBe(true);
     expect(checkRow([1, 1, 0], 1)).toBe(false);
+});
+
+test('checkRows', () => {
+    expect(checkRows([
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 0, 0],
+    ])).toEqual(1);
+});
+
+test('check', () => {
+    expect(check([
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 0, 0],
+    ], 6)).toEqual(1);
 });
 
 test('renders correctly', () => {
